862faa5bc60f902eb1e7fdf06325832ebb571d46
Per5eu5 <pavel7557@hotmail.com>
2019-07-19 16:33:22 +0300

    Добавлены теги

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
 from django.core.mail import send_mail
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
+from taggit.models import Tag
 
 
 class PostListView(ListView):
@@ -13,8 +14,14 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3)  # по 3 поста на каждой странице
     page = request.GET.get('page')
     try:
@@ -25,7 +32,7 @@ def post_list(request):
     except EmptyPage:
         # Если страница вне диапазона отправляем последнюю страницу результатов
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -34,7 +41,25 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+
+    # Список активных комментариев к этой записи
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        # Комментарий отправлен
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Создать объект комментариев, но еще не сохранять его в базе данных
+            new_comment = comment_form.save(commit=False)
+            # Связать текущий пост с комментарием
+            new_comment.post = post
+            # Сохранить комментарий в базе данных
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment,
+                                                     'comment_form': comment_form})
 
 
 def post_share(request, post_id):
